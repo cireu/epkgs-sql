05a27c3a45623539b142514f20b95ab90f63c38b
Zhu Zihao <all_but_last@163.com>
2020-04-12 23:41:42 +0800

    init: Restart our repository.

---
+++ github_action_script.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd main || exit 1
+
+SOURCE="../epkgs/epkg.sqlite"
+DIST="epkg.sql"
+
+test -f "${SOURCE}" || exit 1
+
+git config --local user.email "action@github.com"
+git config --local user.name "Github Action"
+git remote set-url origin "https://${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+
+sudo apt-get install sqlite3
+
+# Workaround: sqlite3 dump does nothing for PRAGMAs, but user_version
+# is needed for epkg to verify the database.
+USER_VERSION=$(sqlite3 "${SOURCE}" "PRAGMA user_version;")
+echo "PRAGMA user_version=${USER_VERSION};" > "${DIST}"
+sqlite3 "${SOURCE}" .dump >> "${DIST}"
+
+git add epkg.sql
+
+# If something changed, push.
+if git commit -m "Sync from https://github.com/emacsmirror/epkgs"; then
+    git push --quiet
+fi
